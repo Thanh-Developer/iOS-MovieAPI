380e156478a5a27a347db29eb3e29699136f795e
JohnPham <ThanhP@joblogic.com>
2023-10-29 23:27:00 +0700

    first commit

---
+++ MovieAPI/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  MovieAPI
+//
+//  Created by JohnPham on 10/29/23.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
