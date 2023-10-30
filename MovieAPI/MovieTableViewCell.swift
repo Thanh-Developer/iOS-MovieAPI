61332a9b4de02ea064b520fcc40c2f7e25bda34a
JohnPham <ThanhP@joblogic.com>
2023-10-30 22:56:37 +0700

    Format code

--- MovieAPI/MovieTableViewCell.swift
+++ MovieAPI/MovieTableViewCell.swift
@@ -8,26 +8,26 @@
 import UIKit
 
 class MovieTableViewCell: UITableViewCell {
-
+    
     @IBOutlet var movieTitleLabel: UILabel!
     @IBOutlet var movieYearLabel: UILabel!
     @IBOutlet var moviePosterImageView: UIImageView!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-
+    
     static let identifier = "MovieTableViewCell"
-
+    
     static func nib() -> UINib {
         return UINib(nibName: "MovieTableViewCell",
                      bundle: nil)
     }
-
+    
     func configure(with model: Movie) {
         self.movieTitleLabel.text = model.Title
         self.movieYearLabel.text = model.Year
